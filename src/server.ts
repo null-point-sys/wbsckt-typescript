7b3a7949318cca5daaa43c33216f46a5b5bab38a
null-point-sys <jaalza@gmail.com>
2020-04-30 09:23:42 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import * as express from "express";
 import router from './router';
-import http from 'http';
-//import bodyParser from 'body-parser';
+import * as bodyParser from 'body-parser';
 //import cors from 'cors';
 
 import log from "./log";
