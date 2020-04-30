f885d45d39c8a3d1582ad4af95b2407790c3cba4
null-point-sys <jaalza@gmail.com>
2020-04-30 00:46:31 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
 import * as express from "express";
+import router from './router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
 import log from "./log";
 
 const app = express();
+
+// BodyParser
+app.use( bodyParser.urlencoded({ extended: true }) );
+app.use( bodyParser.json() );
+
+// CORS
+app.use( cors({ origin: true, credentials: true  }) );
+
+
+// Rutas de servicios
+server.app.use('/', router );
 
 app.get("/", (req, res) => {
     res.json(JSON.stringify({ok: 1})).end();
