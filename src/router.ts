4f15c4ac4108f2436e36da87bdabd22357fde6c2
aZapata <jaalza@gmail.com>
2020-06-22 16:50:16 -0500

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { UsuariosLista } from "usuarios-lista";
+import { UsuariosLista } from "./usuarios-lista";
 const router = Router();
 
 
