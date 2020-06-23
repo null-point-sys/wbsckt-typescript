79f008a4436435138ccfef5001349b0862af1772
aZapata <jaalza@gmail.com>
2020-06-22 20:16:45 -0500

    Create block.ts

---
+++ src/block.ts
@@ -0,0 +1,23 @@
+class Block{                      
+  constructor(timestamp, transactions, previousHash = ''){        
+    this.timestamp     = timestamp;    
+    this.transactions  = transactions;
+    this.previousHash  = previousHash;
+    this.hash          = this.calculateBlockHash();
+  }
+  calculateBlockHash(){
+   	// need NPM crypto-js
+   	return SHA256(
+   		this.timestamp 
+   		+ JSON.stringify(this.transactions) 
+   		+ this.previousHash
+    ).toString();
+  }
+  /* sign
+  hasValidTransactions(){
+    for (const Tx of this.transactions){
+      if(!Tx.isValid()){ return false }
+    }
+    return true;
+  }  // signature     */ 
+}
