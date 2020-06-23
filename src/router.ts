d801638cc9ea0dd63571d742b5ec87d9d915ff2b
aZapata <jaalza@gmail.com>
2020-06-22 19:26:34 -0500

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -29,12 +29,12 @@ router.post('/crearusuario', ( req: Request, res: Response  ) => {
     res.json( usuarioLista.getUsuario(n) ); //ok
 });
 
-/* router.get('/usuarios/detalle', (  req: Request, res: Response ) => {
+router.get('/usuarios/detalle', (  req: Request, res: Response ) => {
     res.json({
         ok: true,
         clientes: usuarioLista.getLista()
     });
-}); */
+});
 
 router.get('/mensajes', ( req: Request, res: Response  ) => {
     res.json({
